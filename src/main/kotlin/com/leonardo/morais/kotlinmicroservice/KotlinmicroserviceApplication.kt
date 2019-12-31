4efd2b8904b79f663b2d3bb314ca85e48a48965e
Leonardo Broch de Morais <leo_broch@hotmail.com>
2019-12-31 18:05:48 -0200

    Initial project

---
+++ src/main/kotlin/com/leonardo/morais/kotlinmicroservice/KotlinmicroserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.leonardo.morais.kotlinmicroservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinmicroserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinmicroserviceApplication>(*args)
+}
---
+++ src/main/resources/db/changelog/sprint-1/account/createtable-account-ddl.sql
@@ -0,0 +1,6 @@
+CREATE TABLE ACCOUNT(
+    account_id INT NOT NULL,
+    username VARCHAR(50),
+    email VARCHAR(30),
+    PRIMARY KEY (account_id)
+)
